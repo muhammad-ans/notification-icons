bb9e72f6dd2342387d36197ffb4c32be03a3b91a
Ans <muhammad_ans@tutanota.com>
2024-10-28 04:32:37 +0500

    added preferneces for DND mode

--- notification-icons@muhammad_ans.github/prefs.js
+++ notification-icons@muhammad_ans.github/prefs.js
@@ -1,40 +1,68 @@
-import Gio from 'gi://Gio';
-import Adw from 'gi://Adw';
+import Gio from "gi://Gio";
+import Adw from "gi://Adw";
+import Gtk from "gi://Gtk";
 
-import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
-
+import {
+  ExtensionPreferences,
+  gettext as _,
+} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class ExamplePreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        // Create a preferences page, with a single group
-        const page = new Adw.PreferencesPage({
-            title: _('General'),
-            icon_name: 'dialog-information-symbolic',
-        });
-        window.add(page);
+  fillPreferencesWindow(window) {
+    const page = new Adw.PreferencesPage({
+      title: _("General"),
+      icon_name: "dialog-information-symbolic",
+    });
+    window.add(page);
 
-        const group = new Adw.PreferencesGroup({
-            title: _('Configuration'),
-            description: _('Configure the the extension'),
-        });
-        page.add(group);
+    const group = new Adw.PreferencesGroup({
+      title: _("Configuration"),
+      description: _("Configure the extension"),
+    });
+    page.add(group);
 
-        // Create a new preferences row
-        const row = new Adw.SwitchRow({
-            title: _('Right Side'),
-            subtitle: _('Whether to show icons on the right side'),
-        });
-        group.add(row);
+    const row = new Adw.SwitchRow({
+      title: _("Right Side"),
+      subtitle: _("Whether to show icons on the right side"),
+    });
+    group.add(row);
 
-        const  row2 = new Adw.SwitchRow({
-            title: _('Colored Icons'),
-            subtitle: _('Toggles between symbolic and colored icons'),
-        });
-        group.add(row2);
+    const row2 = new Adw.SwitchRow({
+      title: _("Colored Icons"),
+      subtitle: _("Toggles between symbolic and colored icons"),
+    });
+    group.add(row2);
 
-        // Create a settings object and bind the row to the `show-indicator` key
-        window._settings = this.getSettings();
-        window._settings.bind('right-side', row, 'active',Gio.SettingsBindFlags.DEFAULT);
-        window._settings.bind('colored-icons', row2, 'active',Gio.SettingsBindFlags.DEFAULT);
-    }
+    const options = new Gtk.StringList();
+    options.append(_("Always"));
+    options.append(_("Urgent"));
+    options.append(_("Never"));
+
+    const comboRow = new Adw.ComboRow({
+      title: _("DND Mode"),
+      subtitle: _("Show icons when do no disturb is on"),
+      model: options,
+    });
+    group.add(comboRow);
+
+    window._settings = this.getSettings();
+    window._settings.bind(
+      "right-side",
+      row,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT
+    );
+    window._settings.bind(
+      "colored-icons",
+      row2,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT
+    );
+    window._settings.bind(
+      "dnd-mode",
+      comboRow,
+      "selected",
+      Gio.SettingsBindFlags.DEFAULT
+    );
+  }
 }
